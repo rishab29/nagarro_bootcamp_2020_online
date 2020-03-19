71f1cd4ceb52bece9ea2dd5ab6856783c034f1a8
rishab29 <rishab.kapoor.29@gmail.com>
2020-03-19 15:01:35 +0530

    Code For Session 16

---
+++ src/Lecture16/StackDemo.java
@@ -0,0 +1,21 @@
+package Lecture16;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Stack<Integer> stack=new Stack<>();
+       
+       for(int i=1;i<=5;i++) {
+    	   stack.push(i*10);
+       }
+       
+       while(!stack.isEmpty()) {
+    	   System.out.println(stack.peek());
+    	   System.out.println(stack.pop());
+       }
+	}
+
+}
